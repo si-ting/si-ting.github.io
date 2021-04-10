f6b47ed15e8817d79cec0b941d84d68f4307a263
si-ting <82227575+si-ting@users.noreply.github.com>
2021-04-10 13:16:15 +0800

    v2

--- 3d.js
+++ 3d.js
@@ -1,22 +1,24 @@
-var photosDom = document.getElementById('photos');
-//获取图片数组
-var images = photosDom.getElementsByTagName('img'); 
-//获取图片数量
-var len = images.length;        
-//计算每张图片按Y轴旋转的角度 
-var deg = 360 / len//Math.floor();                    
-
-
-photosDom.style.transform = "rotateX(-5deg)";
-var x = 0;
-var count = 0;
-setInterval(function(){
-	for(var i = 0; i < len;i++){
-		images[i].style = 'transform : rotateY(' + (deg * i + deg * x / 25 - 11.25)%360 +'deg) translateZ(500px)'; //deg前面不要加空格
-		
+window.addEventListener('DOMContentLoaded', function(){
+	var container = document.getElementById('container');
+	var images = container.getElementsByTagName('img'); 
+	var images_num = images.length;        
+	var deg = 360 / images_num; // 计算每张图片按Y轴旋转的角度
+	for(var i = 0; i < images_num;i++){ // 初始化位置
+		images[i].style.transform = ' rotateY(' + (deg * i - 11.25) % 360 + 'deg) translateZ(500px)'; //deg前面不要加空格
 	}
-	count++;
-	if(count > 550) x++;
-	if(count == 600) count = 0;
-    //photosDom.style.transform = "rotateX(-10deg) rotateY("+ (x++) * 1.2 +"deg)";
-},25);
+	var x = 0;
+	var count = 0;
+	setInterval(function(){
+		// 原来的方法：容器不动，改变每个图片的Y旋转角度
+		// for(var i = 0; i < images_num;i++){ 
+		// 	images[i].style.transform = 'rotateY(' + (deg * i - 11.25 + deg * x / 25) % 360 + 'deg) translateZ(500px)'; //deg前面不要加空格
+		// }
+		// 现在的方法：改变容器的Y旋转角度
+		container.style.transform = 'rotateX(-5deg) rotateY(' + (deg * x / 25) % 360 + 'deg)';
+		count++;
+		if(count > 550) 
+			x++;
+		if(count == 600) 
+			count = 0;
+	}, 25);
+});
